4c0c9070c9da2fb392fddcd787c8b0d1789f42b5
geraldpinkney <Oracle12>
2022-03-14 09:54:14 +0000

    updates for testing and starting to build play

--- Calculus.py
+++ Calculus.py
@@ -218,7 +218,7 @@ class Player:
 
 
 class Game:
-    def __init__(self, deck=FrenchDeck(), numOfPlayers=2, numOfRounds=10):
+    def __init__(self, deck=FrenchDeck('Y'), numOfPlayers=2, numOfRounds=10):
 
         self._deck = deck
         self._deck.shuffle()
@@ -501,4 +501,18 @@ class Trick:
 
 class InterfaceLayer:
     def __init__(self):
-        current_menu = 'Main'
+        current_menu = None
+        last_menu = None
+        current_game = None
+
+    def main_menu(self):
+        self.last_menu = self.current_menu
+        self.current_menu = 'main_menu'
+        print(f'select options: \n\t1-NewGame\n\t2-Exit')
+        choice = input('Enter option number: ')
+        if choice == 1:
+            num_of_players = input('Enter number of players: ')
+            num_of_rounds = input('Enter number of rounds: ')
+            self.current_game = Game(FrenchDeck(), num_of_players, num_of_rounds)
+        else:
+            exit()
