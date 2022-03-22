67759e19eb37af8086ef70fc40e923cc441dbeac
gerald <geraldpinkney@gmail.com>
2022-03-22 13:19:00 +0000

    added comparisons
    Signed-off-by: gerald <geraldpinkney@gmail.com>

--- fluentPython/Calculus/Player.py
+++ fluentPython/Calculus/Player.py
@@ -1,5 +1,6 @@
 from fluentPython.Calculus.Hand import Hand
 
+
 class Player:
     def __init__(self, name):
         self._score = 100
@@ -16,4 +17,41 @@ class Player:
         return f'Player: {self.name}, Score: {self._score}, Hand: {self.hand}'
 
     def __repr__(self):
-        return f'Player: {self.name}, Score: {self._score}, Hand: {self.hand}'
+        # f'Player: {self.name}, Score: {self._score}, Hand: {self.hand}'
+        return f'Player: {self.name}'
+
+    def __eq__(self, other):
+        if self.name == other.name:
+            return True
+        else:
+            return False
+
+    def __ne__(self, other):
+        if self.name == other.name:
+            return False
+        else:
+            return True
+
+    def __lt__(self, other):
+        if self._score < other._score:
+            return True
+        else:
+            return False
+
+    def __le__(self, other):
+        if self._score <= other._score:
+            return True
+        else:
+            return False
+
+    def __gt__(self, other):
+        if self._score > other._score:
+            return True
+        else:
+            return False
+
+    def __le__(self, other):
+        if self._score >= other._score:
+            return True
+        else:
+            return False
